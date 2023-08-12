d38f1577322099c5c3b373ebe38ec2470796b553
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-08-12 18:41:55 +0600

    add-redux-logger-library

--- src/Redux/Middleware/Mylogger.js
+++ src/Redux/Middleware/Mylogger.js
@@ -1,4 +1,9 @@
+import rootReducer from "../RootReducer/RootReducer";
+
 const myLogger=(store)=>(next)=>(action)=>{
+    console.log(`Action Types: ${JSON.stringify(action)}`);
+    console.log(`Before State: ${JSON.stringify(store.getState())}`);
+    console.log(`Upcoming State: ${JSON.stringify([action].reduce(rootReducer, store.getState()))}`)
 
     return next(action);
 }
--- src/Redux/Store/Store.js
+++ src/Redux/Store/Store.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "../RootReducer/RootReducer";
 import myLogger from "../Middleware/Mylogger";
+import logger from "redux-logger"
 
-const store = createStore(rootReducer,applyMiddleware(myLogger))
+const store = createStore(rootReducer,applyMiddleware(logger,myLogger))
 
 
 export default store;
